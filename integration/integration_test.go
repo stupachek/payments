5d9dbf6e76e2d5f16f569fd1804de0a2e8db57bf
stupachek <alona.stupak@gmail.com>
2023-02-25 20:37:09 +0200

    test for block-unblock

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -751,6 +751,59 @@ func TestPaymentIntegration(t *testing.T) {
 		}
 
 	})
+	t.Run("blockUnblock", func(t *testing.T) {
+		inputBob := controllers.RegisterInput{
+			FisrtName: "Bob",
+			LastName:  "Lee",
+			Email:     "lee.bee@gmail.com",
+			Password:  "qwerty",
+		}
+		reqResult := sendReq(t, "POST", "http://localhost:8080/users/register", inputBob, nil)
+		if _, ok := reqResult["message"]; !ok {
+			t.Fatal("error register")
+		}
+		var UUIDBob string = reqResult["uuid"].(string)
+		reqResult = sendReq(t, "POST", "http://localhost:8080/users/login", inputBob, nil)
+		tokenBob, ok := reqResult["token"].(string)
+		if !ok {
+			t.Fatal("error login")
+		}
+		url := fmt.Sprintf("http://localhost:8080/users/%v/accounts/new", UUIDBob)
+		auth := make(map[string]string)
+		auth["Authorization"] = tokenBob
+		reqResult = sendReq(t, "POST", url, nil, auth)
+		if _, ok := reqResult["message"]; !ok {
+			t.Fatal("create account error")
+		}
+		accountUUID := reqResult["uuid"].(string)
+		url = fmt.Sprintf("http://localhost:8080/users/%v/accounts/%v/block", UUIDBob, accountUUID)
+		reqResult = sendReq(t, "POST", url, nil, auth)
+		if _, ok := reqResult["message"]; !ok {
+			t.Fatal("block account error")
+		}
+		url = fmt.Sprintf("http://localhost:8080/users/%v/accounts/%v/unblock", UUIDBob, accountUUID)
+		reqResult = sendReq(t, "POST", url, nil, auth)
+		if _, ok := reqResult["message"]; !ok {
+			t.Fatal("request to unblock account error")
+		}
+		inputAdmin := controllers.LoginInput{
+			Email:    "admin@admin.admin",
+			Password: "admin",
+		}
+		reqResult = sendReq(t, "POST", "http://localhost:8080/users/login", inputAdmin, nil)
+		tokenAdmin, ok := reqResult["token"].(string)
+		if !ok {
+			t.Fatal("error login")
+		}
+		adminUUID := reqResult["uuid"].(string)
+		url = fmt.Sprintf("http://localhost:8080/admin/%v/accounts/%v/unblock", adminUUID, accountUUID)
+		auth["Authorization"] = tokenAdmin
+		reqResult = sendReq(t, "POST", url, nil, auth)
+		if _, ok := reqResult["message"]; !ok {
+			t.Fatal("unblock account error")
+		}
+
+	})
 
 }
 
